7ca0b599ebef0fae43f6817f48ce108a75e87c91
yoonkeen <agy91521947@gmail.com>
2025-03-22 18:33:50 +0900

    project folder refix

---
+++ app/views/Main/MainPage.tsx
@@ -0,0 +1,12 @@
+import CustomButton from './components/CustomButton';
+
+const MainPage = () => {
+  return (
+    <div>
+      <h2>Main Page</h2>
+      <CustomButton />
+    </div>
+  );
+};
+
+export default MainPage;
